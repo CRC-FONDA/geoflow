5800ba5dccbff06102c6729d60531560b5f7faef
Florian-Katerndahl <florian@katerndahl.com>
2022-01-20 12:52:24 +0100

    added missing semicolons

--- run_nf.sh
+++ run_nf.sh
@@ -17,6 +17,7 @@ if [ "$#" -gt 0 ]; then
 			;;
 		r)
 			nextflow run -resume main.nf --input_dirP='/data/Dagobah/dc/deu/ard/X0061_Y0048/*{BOA,QAI}.tif' --output_dir_indices=/data/Dagobah/fonda/shk/test_out
+			;;
 		*)
 			echo "Invalid argument provided"
 			exit 1
