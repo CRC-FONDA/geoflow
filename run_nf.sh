53f0d4bcadae28736fb62cc5c2bfcb839a629fe1
Florian-Katerndahl <florian@katerndahl.com>
2022-01-20 12:48:57 +0100

    Running WF is now its own option

--- run_nf.sh
+++ run_nf.sh
@@ -15,12 +15,11 @@ if [ "$#" -gt 0 ]; then
 			echo ""
 			git pull
 			;;
+		r)
+			nextflow run -resume main.nf --input_dirP='/data/Dagobah/dc/deu/ard/X0061_Y0048/*{BOA,QAI}.tif' --output_dir_indices=/data/Dagobah/fonda/shk/test_out
 		*)
 			echo "Invalid argument provided"
 			exit 1
 		esac;
 	done
 fi
-
-nextflow run -resume main.nf --input_dirP='/data/Dagobah/dc/deu/ard/X0061_Y0048/*{BOA,QAI}.tif' --output_dir_indices=/data/Dagobah/fonda/shk/test_out
-
