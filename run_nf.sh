fa5ec0df09893f3a46fc34e9e9f69e88b4a2fea3
Florian-Katerndahl <florian@katerndahl.com>
2022-01-21 13:46:10 +0100

    added new cli option and renamed flag when running wf

--- run_nf.sh
+++ run_nf.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 if [ "$#" -gt 0 ]; then
-	while getopts 'dgr' opt; do
+	while getopts 'dgnv' opt; do
 		case "$opt" in
 		d)
 			echo "Pulling latest docker image from dockerhub:"
@@ -15,8 +15,12 @@ if [ "$#" -gt 0 ]; then
 			echo ""
 			git pull
 			;;
-		r)
+		n)
 			nextflow run -resume main.nf --input_dirP='/data/Dagobah/dc/deu/ard/X0061_Y0048/*{BOA,QAI}.tif' --output_dir_indices=/data/Dagobah/fonda/shk/test_out
+			;;
+		v)
+			nextflow run main.nf --input_dirP='/data/Dagobah/dc/deu/ard/X0061_Y0048/*{BOA,QAI}.tif' --output_dir_indices=/data/Dagobah/fonda/shk/test_out -with-dag
+			 flowchart.svg
 			;;
 		*)
 			echo "Invalid argument provided"
