16e4091e4c96d68661cff5127265f69cd699dcd4
Florian-Katerndahl <florian@katerndahl.com>
2022-04-04 11:08:03 +0200

    rephrasing of description, updated dict-comprehension, removed dead code

--- scripts-python/full_stack_explode.py
+++ scripts-python/full_stack_explode.py
@@ -1,12 +1,11 @@
 #! /usr/bin/python3
 from osgeo import gdal
 import argparse
-from pathlib import Path
 from typing import Dict, List, Any
 from python_modules import io
 
 parser = argparse.ArgumentParser(
-	description="While similar/almost identical to 'explode.py' in functionality, this is script is used for the creation on the 'final' data cube and will "
+	description="While similar/almost identical to 'explode.py' in functionality, this is script is used for the creation on the 'final' data cube and will\n"
 				"likely be merged into 'explode.py' once I (re-)figured out how to solve the problem at hand.\n"
 				"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
 				"Before extracting features and any training/prediction can be done, a 'final' data cube needs to be created in which all inputs (i.e. "
@@ -19,13 +18,8 @@ parser.add_argument("--input_files", nargs='+', type=str, required=True,
 parser.add_argument("--out_name", nargs=1, type=str, required=True,
 					help="Name of output VRT-cube")
 
-args: Dict[str, Any] = {key: (value[0] if isinstance(value, List) else value) for key, value in vars(parser.parse_args()).items()}
+args: Dict[str, Any] = {key: (value[0] if key == "out_name" else value) for key, value in vars(parser.parse_args()).items()}
 final_cube_inputs: List[str] = list()
-
-#files: List[Path] = args.get("input_dir").glob("*.[tif][vrt]")
-
-# drop BOA.tif files as they are not needed in further stacking
-#files = list(filter(lambda x: "BOA.tif" not in x.name, files))
 
 # BOA vrt files don't need to be re-processed
 filtered_files: List[str] = list(filter(lambda x: "BOA" not in x, args.get("input_files")))
@@ -44,6 +38,6 @@ for raster_file in filtered_files:
 
 	io.close_gdal(raster)
 
-final_cube_inputs.sort() # TODO does this sufficiently address the need for cross-tile aligned layers?
+final_cube_inputs.sort() # TODO do I need to sort the stack? Method discussion needed from my side!
 
 io.create_big_cube(final_cube_inputs, args.get("out_name"))
