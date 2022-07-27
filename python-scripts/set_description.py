a10f13f4e49c63429c18fc6508a1185e3b865254
Florian-Katerndahl <florian@katerndahl.com>
2022-07-27 11:48:31 +0200

    format script with black to py38 target

--- python-scripts/set_description.py
+++ python-scripts/set_description.py
@@ -9,14 +9,26 @@ parser = argparse.ArgumentParser(
     description="This program opens a raster file and updates the band descriptions for all layers specified.\
     Inspiration from https://gis.stackexchange.com/a/290806"
 )
-parser.add_argument("-i", "--input_file", nargs=1, type=str, required=True, dest="input_file",
-                    help="File path to input dataset.")
-parser.add_argument("--names", nargs="+", type=str, required=True,
-                    help="Map of old and new names in the form of BAND-INDEX=NEW-NAME")
+parser.add_argument(
+    "-i",
+    "--input_file",
+    nargs=1,
+    type=str,
+    required=True,
+    dest="input_file",
+    help="File path to input dataset.",
+)
+parser.add_argument(
+    "--names",
+    nargs="+",
+    type=str,
+    required=True,
+    help="Map of old and new names in the form of BAND-INDEX=NEW-NAME",
+)
 
 args: Dict[str, List[str]] = vars(parser.parse_args())
 
-temporary_names = {'names': cio.dict_from_string_list(args.get('names'))}
+temporary_names = {"names": cio.dict_from_string_list(args.get("names"))}
 args.update(temporary_names)
 del temporary_names
 
